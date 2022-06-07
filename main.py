3a097307b6007fa937072a17d435d9edc9f03dd3
oshiqqq <oshisl@mail.ru>
2022-06-08 00:42:46 +0500

    Commit

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ def paste(a:list, b):
         a.insert((len(a)//2)+1, b)
     return a
 print(paste(str, s))
+
+#2) необходимо разработать функцию, принимающую список строк, число и
+#строку, и выполняющую вставку строки в указанную позицию, если вставка 
+#на указанную позицию невозможна, то вернуть сообщение «paste operation is
+#not possible»;
+s = input("Введите строку: ")
+index = int(input("Введите позицию: "))
+b = input("Введите строку: ")
+def insert_dash(s, index):
+    if index > len(s):
+        return print("paste operation is not possible")
+    return s[:index] + b + s[index:]
+print(insert_dash(s, index))
