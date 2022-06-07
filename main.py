258043e538d92590c627770c95f9a1e381da6bb2
Your Name <your_email@whatever.com>
2022-06-08 01:08:22 +0500

    Commit

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ def paste(a:list, b):
         a.insert((len(a)//2)+1, b)
     return a
 print(paste(str, s))
+
+#3) необходимо разработать функцию, принимающую список строк и число, и
+#выполняющую удаление строки по указанной позицию списка, если удаление
+#невозможно, то вернуть сообщение «delete operation is not possible».
+s = list(input("Введите строку: "))
+print(s[2])
+a = int(input("Введите позицию: "))
+def str(s, a):
+    if a > len(s):
+        return print("delete operation is not possible")
+    del s[a]
+    return "".join(s)
+print(str(s, a))
